3df6fb1d46316c33fd62f6a530a97e94391d6c57
Rihard <rihard.liiva@gmail.com>
2020-06-22 00:40:06 +0300

    initial commit

---
+++ src/main/java/ee/rihardliiva/dictionaryback/DictionaryBackendApplication.java
@@ -0,0 +1,13 @@
+package ee.rihardliiva.dictionaryback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionaryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/rihardliiva/dictionaryback/DictionaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.rihardliiva.dictionaryback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
