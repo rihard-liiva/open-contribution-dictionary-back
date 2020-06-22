14cd280f5b3ef1a25c389c92d8c18dfeb2e09581
Rihard <rihard.liiva@gmail.com>
2020-06-22 16:30:04 +0300

    create models and repositories for dict entry and language

--- src/main/java/ee/rihardliiva/dictionaryback/DictionaryBackendApplication.java
+++ src/main/java/ee/rihardliiva/dictionaryback/DictionaryBackendApplication.java
@@ -1,13 +1,39 @@
 package ee.rihardliiva.dictionaryback;
 
+import ee.rihardliiva.dictionaryback.dictionaryentry.model.DictionaryEntry;
+import ee.rihardliiva.dictionaryback.dictionaryentry.repository.DictionaryEntryRepository;
+import ee.rihardliiva.dictionaryback.language.model.Language;
+import ee.rihardliiva.dictionaryback.language.repository.LanguageRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class DictionaryBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DictionaryBackendApplication.class, args);
+	}
+
+	//Create dummy data
+	@Bean
+	public CommandLineRunner createEntries(DictionaryEntryRepository dictionaryEntryRepository) {
+
+		Language est = new Language("EST");
+		Language eng = new Language("ENG");
+		Language rus = new Language("RUS");
+
+		return (args) -> {
+			List<DictionaryEntry> words = List.of(
+					new DictionaryEntry("Tere", "Hello", est),
+					new DictionaryEntry("Tere", "Hello", est),
+					new DictionaryEntry("Laud", "Table", est),
+					new DictionaryEntry("Tool", "Chair", est));
+			dictionaryEntryRepository.saveAll(words);
+		};
 	}
 
 }
---
+++ src/main/java/ee/rihardliiva/dictionaryback/dictionaryentry/model/DictionaryEntry.java
@@ -0,0 +1,27 @@
+package ee.rihardliiva.dictionaryback.dictionaryentry.model;
+
+import ee.rihardliiva.dictionaryback.language.model.Language;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+public class DictionaryEntry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String word;
+    @NonNull
+    private String equivalent;
+    @NonNull
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Language originatingLanguage;
+}
