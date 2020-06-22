88c008189e96f7c64f63815b95010d492aa4ec0a
Rihard <rihard.liiva@gmail.com>
2020-06-22 16:39:40 +0300

    create dict entry controller

---
+++ src/main/java/ee/rihardliiva/dictionaryback/dictionaryentry/controller/DictionaryEntryController.java
@@ -0,0 +1,14 @@
+package ee.rihardliiva.dictionaryback.dictionaryentry.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600, allowedHeaders = "*")
+@RestController
+@RequestMapping("/entry")
+public class DictionaryEntryController {
+
+
+
+}
